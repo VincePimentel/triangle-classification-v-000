352a5ff6a6217fc03e4845e7ff73fd196087c976
Vince Andrew Pimentel <contact@vincep.me>
2018-10-27 22:26:14 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,6 +8,16 @@ class Triangle
   end
 
   def kind
-    if
+    if a != 0 || b != 0 || c != 0
+      if a == b || a == c || b == c
+        return :isosceles
+      elsif a != b && a != c && b != c
+        return :scalene
+      elsif a == b && a == c && b == c
+        return :equilateral
+      end        
+    else
+      
+    end
   end
 end
