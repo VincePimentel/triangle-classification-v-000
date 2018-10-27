9a27eee0820bc2e3b305fc60181f91e5cd537d9e
Vince Andrew Pimentel <contact@vincep.me>
2018-10-27 22:30:24 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,9 +11,9 @@ class Triangle
     if a != 0 || b != 0 || c != 0
       if a == b || a == c || b == c
         return :isosceles
-      elsif a != b && a != c && b != c
+      elsif a != b && a != c
         return :scalene
-      elsif a == b && a == c && b == c
+      elsif a == b && a == c
         return :equilateral
       end
     else
