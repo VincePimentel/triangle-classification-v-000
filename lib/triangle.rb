5e2bf42188093f2aeb3805a984e866843f333ff7
Vince Andrew Pimentel <contact@vincep.me>
2018-10-28 00:22:36 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,12 +13,6 @@ class Triangle
   def valid_triangle?
     if @@triangle[0].to_f + @@triangle[1].to_f > @@triangle[2].to_f || @@triangle[1].to_f + @@triangle[2].to_f > @@triangle[0].to_f || @@triangle[0].to_f + @@triangle[2].to_f > @@triangle[1].to_f
       return true
-    elsif @@triangle.include?(0)
-      return false
-    elsif @@triangle[0] < 0
-      return false
-    else
-      return false
     end
   end
 
