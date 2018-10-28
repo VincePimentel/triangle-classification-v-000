24dddef0e24e222ffb7b002010342ba9854b39bb
Vince Andrew Pimentel <contact@vincep.me>
2018-10-28 00:24:10 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -37,7 +37,7 @@ class Triangle
       :isosceles
     elsif scalene?
       :scalene
-    elsif valid_triangle? != true
+    elsif !valid_triangle?
       begin
         raise TriangleError
       rescue TriangleError => error
